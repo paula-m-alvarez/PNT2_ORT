761de7c021a20a095f2ee27a515d716f4531549a
paula-m-alvarez <alvarez.paula.maria@gmail.com>
2024-09-02 21:03:11 -0300

    First changes in react project

---
+++ react-project/src/User/User.jsx
@@ -0,0 +1,22 @@
+import './User.css';
+
+function User(props){
+    return(
+        <li className='user-item'>
+            <div className='card user-item__content'>
+                <a href='/'>
+                    <div className='user-item__image avatar'>
+                        <img src={props.Picture} alt={props.Username}/>
+                    </div>
+                    <div className='user-item__info'>
+                        <h2>{ props.UserName }</h2>
+                        <h2>{ props.Title }</h2>
+                        <h3>{ props.city }</h3>
+                    </div>
+                </a>
+            </div>
+        </li>
+    );
+}
+
+export default User;
