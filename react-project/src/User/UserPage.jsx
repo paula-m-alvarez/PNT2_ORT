afe7bf5bc36eed4566e507375572cfb9a7ae03df
paula-m-alvarez <alvarez.paula.maria@gmail.com>
2024-09-09 20:50:13 -0300

    Se agrego el hook de useEffect

--- react-project/src/User/UserPage.jsx
+++ react-project/src/User/UserPage.jsx
@@ -1,16 +1,19 @@
 import UserList from "./UserList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const UserPage = () =>{
     const [users, setUsers] = useState([]);
 
-    fetch("https://raw.githubusercontent.com/ORT-PabloFernandez/PNTP2-REACT-EJEMPLO/main/src/data/Users.json")
+    useEffect(() => {  
+        fetch("https://raw.githubusercontent.com/ORT-PabloFernandez/PNTP2-REACT-EJEMPLO/main/src/data/Users.json")
         .then(response => response.json())
         .then(data => {
             console.log(data);
             setUsers(data);
         })
         .catch(error => console.log(error));
+    }, []);
+    
 
     return(
         <div>
