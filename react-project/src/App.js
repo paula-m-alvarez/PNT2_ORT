1d323aae87106a3923f84da2c0681b6d15b7999c
paula-m-alvarez <alvarez.paula.maria@gmail.com>
2024-09-09 21:05:29 -0300

    Ruteo + UserDetails

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import UserPage from './User/UserPage.jsx';
+import { BrowserRouter, Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-          <h3> Listado de Usuarios </h3>
-          <UserPage />
-    </div>
+    // <div className="App">
+    //       <h3> Listado de Usuarios </h3>
+    //       <UserPage />
+    // </div>
+    <BrowserRouter>
+      <Router>
+        <Router Path="/" element = {<h1>Home</h1>} />
+        <Router Path="/users" element = { <UserPage />} />
+        <Router Path="/users/:id" element = { <UserDetails /> } />
+        <Router Path="/login" element = {<h1> Login </h1>} />
+      </Router>
+    </BrowserRouter>
   );
 }
 
